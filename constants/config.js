3f2153c4e35047b06a3e740acb5e412aa3af2eb5
Asmita Yadav <asmitayadav2003@gmail.com>
2025-05-11 19:57:38 +0530

    Add admin user actions with auto-refresh support

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+     "https://chatapp-frontend-omega-plum.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
